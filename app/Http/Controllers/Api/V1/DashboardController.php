573a353aef159d4a43e4f1d8578dc74e9efbecdd
bishoysouser <bishoysouser@gmail.com>
2024-08-19 14:44:34 +0300

    fixed: session api

--- app/Http/Controllers/Api/V1/DashboardController.php
+++ app/Http/Controllers/Api/V1/DashboardController.php
@@ -20,6 +20,7 @@ class DashboardController extends Controller
      */
     public function index(Request $request)
     {
+        \Log::info('Session data: ', session()->all());
         $user = auth()->guard('api')->user();
         $today = Carbon::now();
         $currentYear = $today->year;
