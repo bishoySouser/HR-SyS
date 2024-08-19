e087e38b537427df5fa9734ef777104d1338ccbd
bishoysouser <bishoysouser@gmail.com>
2024-08-19 11:10:48 +0300

    fixed: session admin

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,6 +42,17 @@ class Kernel extends HttpKernel
 
         ],
 
+        'admin' => [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            // EnsureFrontendRequestsAreStateful::class,
+
+        ],
+
         'api' => [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
