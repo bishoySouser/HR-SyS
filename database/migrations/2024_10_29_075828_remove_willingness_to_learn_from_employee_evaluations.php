853574037c1b703ea745a08b26947ae0b05e070d
bishoysouser <bishoysouser@gmail.com>
2024-10-29 11:05:56 +0300

    update: remove col willingness_to_learn

---
+++ database/migrations/2024_10_29_075828_remove_willingness_to_learn_from_employee_evaluations.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('employee_evaluations', function (Blueprint $table) {
+            $table->dropColumn('willingness_to_learn');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('employee_evaluations', function (Blueprint $table) {
+            $table->enum('willingness_to_learn', ['improvement_required', 'satisfactory', 'good', 'excellent']);
+        });
+    }
+};
